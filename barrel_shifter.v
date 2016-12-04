c2aad7d269eae96f615f63d09d6a79de23fd6440
zlsh80826 <zlsh80826@gmail.com>
2016-12-04 16:08:51 +0800

    rename variable

--- barrel_shifter.v
+++ barrel_shifter.v
@@ -1,14 +1,14 @@
 module BarrelShifter ( input [2:0] ShiftSelect,
 					   input [3:0] ShifterAmount,
-					   input signed [15:0] originB,
-					   output signed [15:0] shiftedB);
+					   input signed [15:0] OriginB,
+					   output signed [15:0] ShiftedB);
 	
 	parameter UNDEFINE = 16'b0000_0000_0000_0000;
 
-	assign shiftedB = ( ShiftSelect == 3'b000 ) ? ( originB >> ShifterAmount ) :
-					  ( ShiftSelect == 3'b001 ) ? ( originB << ShifterAmount ) :
-					  ( ShiftSelect == 3'b010 ) ? ( ( originB >> ShifterAmount ) | ( originB << ~ShifterAmount ) ) :
-					  ( ShiftSelect == 3'b011 ) ? ( ( originB << ShifterAmount ) | ( originB >> ~ShifterAmount ) ) :
-					  ( ShiftSelect == 3'b110 ) ? ( originB >>> ShifterAmount ) : UNDEFINE;
+	assign ShiftedB = ( ShiftSelect == 3'b000 ) ? ( OriginB >> ShifterAmount ) :
+					  ( ShiftSelect == 3'b001 ) ? ( OriginB << ShifterAmount ) :
+					  ( ShiftSelect == 3'b010 ) ? ( ( OriginB >> ShifterAmount ) | ( OriginB << ~ShifterAmount ) ) :
+					  ( ShiftSelect == 3'b011 ) ? ( ( OriginB << ShifterAmount ) | ( OriginB >> ~ShifterAmount ) ) :
+					  ( ShiftSelect == 3'b110 ) ? ( OriginB >>> ShifterAmount ) : UNDEFINE;
 
 endmodule
