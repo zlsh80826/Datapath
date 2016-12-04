6507b87e73d80091853b4488e23a412ad4da237b
zlsh80826 <zlsh80826@gmail.com>
2016-12-04 14:59:27 +0800

    revise top module parameter

--- top.v
+++ top.v
@@ -1,5 +1,15 @@
-module top(DA, AA, BA, MB, FS, SS, SA, MD, RW, CONSTANT, DATA);
+module top ( input [54:0] ControlWord,
+			 input Clock);
 
+	wire [15:0] Data;
+	wire [15:0] A;
+	wire [15:0] B;
+
+	RegisterFile D_reg ( DA, Data, Clock );
+
+	RegisterFile A_reg ( AA, A, Clock );
+
+	RegisterFile B_reg ( BA, B, Clock );
 
 
 
