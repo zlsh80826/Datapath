3f92a80270d2c123fe32263033586bb071bb8aaa
zlsh80826 <zlsh80826@gmail.com>
2016-12-04 16:35:27 +0800

    retab

--- function_unit.v
+++ function_unit.v
@@ -1,31 +1,31 @@
 module FunctionUnit ( input  [15:0] A,
-			 		  input  [15:0] B,
-			 		  output [15:0] Result,
-				 	  input  [3:0]  FunctionSelect,
-			 		  output Overflow,
-			 		  output CarryOut,
-			 		  output Negative,
-			 		  output Zero);
-	
-	parameter UNDEFINE = 16'b0000_0000_0000_0000;	
+                      input  [15:0] B,
+                      output [15:0] Result,
+                      input  [3:0]  FunctionSelect,
+                      output Overflow,
+                      output CarryOut,
+                      output Negative,
+                      output Zero);
+    
+    parameter UNDEFINE = 16'b0000_0000_0000_0000;   
 
-	assign Result = (FunctionSelect == 4'b0000) ? (A) :
-					(FunctionSelect == 4'b0001) ? (A + 4'b0001) :
-					(FunctionSelect == 4'b0010) ? (A + B) :
-					(FunctionSelect == 4'b0011) ? (A + B + 4'b0001) :
-					(FunctionSelect == 4'b0100) ? (A + ~B) :
-					(FunctionSelect == 4'b0101) ? (A + ~B + 4'b0001) :
-					(FunctionSelect == 4'b0110) ? (A - 4'b0001) :
-					(FunctionSelect == 4'b0111) ? (A) : 
-					(FunctionSelect == 4'b1000) ? (A & B) :
-					(FunctionSelect == 4'b1001) ? (A | B) :
-					(FunctionSelect == 4'b1010) ? (A ^ B) :
-					(FunctionSelect == 4'b1011) ? (~A) :
-					(FunctionSelect == 4'b1100) ? (B) : UNDEFINE;
-	
-	assign Overflow = 0;
-	assign CarryOut = 0;
-	assign Negative = 0;
-	assign Zero = 0;
+    assign Result = (FunctionSelect == 4'b0000) ? (A) :
+                    (FunctionSelect == 4'b0001) ? (A + 4'b0001) :
+                    (FunctionSelect == 4'b0010) ? (A + B) :
+                    (FunctionSelect == 4'b0011) ? (A + B + 4'b0001) :
+                    (FunctionSelect == 4'b0100) ? (A + ~B) :
+                    (FunctionSelect == 4'b0101) ? (A + ~B + 4'b0001) :
+                    (FunctionSelect == 4'b0110) ? (A - 4'b0001) :
+                    (FunctionSelect == 4'b0111) ? (A) : 
+                    (FunctionSelect == 4'b1000) ? (A & B) :
+                    (FunctionSelect == 4'b1001) ? (A | B) :
+                    (FunctionSelect == 4'b1010) ? (A ^ B) :
+                    (FunctionSelect == 4'b1011) ? (~A) :
+                    (FunctionSelect == 4'b1100) ? (B) : UNDEFINE;
+    
+    assign Overflow = 0;
+    assign CarryOut = 0;
+    assign Negative = 0;
+    assign Zero = 0;
 
 endmodule 
--- multiplexer.v
+++ multiplexer.v
@@ -1,8 +1,8 @@
 module Multiplexer ( input [15:0] In1,
-					 input [15:0] In2,
-					 input Select,
-					 output [15:0] Result );
+                     input [15:0] In2,
+                     input Select,
+                     output [15:0] Result );
 
-	assign Result = ( Select == 1'b0 ) ? In1 : In2;
+    assign Result = ( Select == 1'b0 ) ? In1 : In2;
 
 endmodule
