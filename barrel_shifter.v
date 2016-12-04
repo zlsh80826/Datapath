0775bc3da10f7ce9f34dbefa86f0a9fc1e0708a8
zlsh80826 <zlsh80826@gmail.com>
2016-12-04 18:01:06 +0800

    barrel_shifter_test_bench

--- barrel_shifter.v
+++ barrel_shifter.v
@@ -1,14 +1,16 @@
+`timescale 1ns/100ps
 module BarrelShifter ( input [2:0] ShiftSelect,
                        input [3:0] ShifterAmount,
-                       input signed [15:0] OriginB,
-                       output signed [15:0] ShiftedB);
+                       input [15:0] OriginB,
+                       output [15:0] ShiftedB);
     
     parameter UNDEFINE = 16'b0000_0000_0000_0000;
-
+           
     assign ShiftedB = ( ShiftSelect == 3'b000 ) ? ( OriginB >> ShifterAmount ) :
                       ( ShiftSelect == 3'b001 ) ? ( OriginB << ShifterAmount ) :
-                      ( ShiftSelect == 3'b010 ) ? ( ( OriginB >> ShifterAmount ) | ( OriginB << ~ShifterAmount ) ) :
-                      ( ShiftSelect == 3'b011 ) ? ( ( OriginB << ShifterAmount ) | ( OriginB >> ~ShifterAmount ) ) :
-                      ( ShiftSelect == 3'b110 ) ? ( OriginB >>> ShifterAmount ) : UNDEFINE;
+                      ( ShiftSelect == 3'b010 ) ? ( ( OriginB >> ShifterAmount ) | ( OriginB << (16 - ShifterAmount) ) ) :
+                      ( ShiftSelect == 3'b011 ) ? ( ( OriginB << ShifterAmount ) | ( OriginB >> (16 - ShifterAmount) ) ) :
+                      ( ShiftSelect == 3'b100 ) ? ( ( OriginB >> ShifterAmount ) | (16'b1000_0000_0000_0000 & OriginB) ) : UNDEFINE;
+    
 
 endmodule
---
+++ barrel_shifter_testbench.v
@@ -0,0 +1,46 @@
+`timescale 1ns/100ps
+
+module BarrelShifterTest;
+
+    reg [15:0] B;
+    wire [15:0] R;
+    reg [2:0] SS;
+    reg [3:0] SA;
+    integer i;
+    
+    BarrelShifter bs( .ShiftSelect(SS),
+                      .ShifterAmount(SA),
+                      .OriginB(B),
+                      .ShiftedB(R) );
+
+    initial begin
+
+        # 5 
+        B = 16'b1101_0000_0000_0000;
+        SA = 4'b0001;
+
+        SS = 3'b000;
+        # 5
+        $display("%b logical right shift %d is %b", B, SA, R);
+
+        SS = 3'b001;
+        # 5
+        $display("%b logical left shift %d is %b", B, SA, R);
+
+        SS = 3'b010;
+        #5
+        $display("%b logical right rotation shift %d is %b", B, SA, R);
+
+        SS = 3'b011;
+        #5
+        $display("%b logical left rotation shift %d is %b", B, SA, R);
+
+        SS = 3'b100;
+        #5
+        $display("%b arithmetical right shift %d is %b", B, SA, R);
+    end
+
+endmodule    
+        
+        
+        
